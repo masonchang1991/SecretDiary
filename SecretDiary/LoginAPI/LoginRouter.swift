46648c2150c00f78ba1d950152ba522a8c4e7408
Mason Chang <masonchang1991@gmail.com>
2017-10-07 19:09:40 +0800

    add login file

---
+++ SecretDiary/LoginAPI/LoginRouter.swift
@@ -0,0 +1,19 @@
+//
+//  LoginRouter.swift
+//  SecretDiary
+//
+//  Created by Ｍason Chang on 2017/10/7.
+//  Copyright © 2017年 Ｍason Chang. All rights reserved.
+//
+
+import Foundation
+
+enum LoginRouter {
+    
+    case facebook
+    
+    case google
+    
+    case email
+    
+}
---
+++ SecretDiary/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SecretDiary
+//
+//  Created by Ｍason Chang on 2017/10/7.
+//  Copyright © 2017年 Ｍason Chang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: LoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+
+
+}
+
